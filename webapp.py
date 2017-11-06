00080926856dcd4d814af62a0be6f8aa2094a541
w326004741 <G00329371@gmit.ie>
2017-11-06 18:36:58 +0000

    add webapp.py file & index.html

--- webapp.py
+++ webapp.py
@@ -1,9 +1,12 @@
-# adapted from:
-# http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+# Adapted from: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+# https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
+# 我使用一个“templates”目录和一个“static”目录。我将所有的.html文件/Flask模板放在templates目录中，而static包含CSS/JS.
+# render_template对通用的html文件工作正常.
 import os
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template #add render_template
 from werkzeug.utils import secure_filename
 
+# change the path
 UPLOAD_FOLDER = '/Users/weichenwang/Year4/Ian/Emerging-Technologies-Project-Year4/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
@@ -31,7 +34,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file', filename=filename))
-    return render_template("index.html")
+    return render_template("index.html") # use render_templated
 
 @app.route('/uploaded_file')
 def uploaded_file():
