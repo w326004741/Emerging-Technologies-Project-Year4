d762ba40beca23528e9fc49177a3370683b32983
w326004741 <G00329371@gmit.ie>
2017-11-06 18:24:53 +0000

    add webapp.py file & index.html

---
+++ webapp.py
@@ -0,0 +1,38 @@
+# adapted from:
+# http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/Users/weichenwang/Year4/Ian/Emerging-Technologies-Project-Year4/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template("index.html")
+
+@app.route('/uploaded_file')
+def uploaded_file():
+    return 'Upload Successful'
